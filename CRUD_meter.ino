29e4fa2ac5c9c643ae7c7849b4e05625bdbc6d80
Dmitry Grigoryev <dmitry.grigoryev@outlook.com>
2016-04-28 21:35:35 +0200

    version 1

---
+++ CRUD_meter.ino
@@ -0,0 +1,171 @@
+
+// Configuration parameters
+const int PIN_IN =   A0;
+const int PIN_1K =   9;
+const int PIN_10K =  11;
+const int PIN_100K = 10;
+const int PIN_1M =   12;
+
+// Calibration parameters
+const float REF_V =    3.5;  // [V]
+const float REF_1K =   1.00; // [kOhm]
+const float REF_10K =  10.0; // [kOhm]
+const float REF_100K = 100;  // [kOhm]
+const float REF_1M =   1000; // [kOhm]
+
+void setup() {
+  Serial.begin(9600); // PC config: stty -F /dev/ttyUSB0 9600 -cooked -crtscts
+  while(!Serial);
+  
+  Serial.println();
+  Serial.print("ADC on pin "); Serial.print(A0); Serial.print(", Vref = "); Serial.print(REF_V); Serial.println("V");  
+  Serial.print("R = "); Serial.print(REF_1K,2); Serial.print("k on pin "); Serial.println(PIN_1K);
+  Serial.print("R = "); Serial.print(REF_10K,1); Serial.print("k on pin "); Serial.println(PIN_10K);
+  Serial.print("R = "); Serial.print(REF_100K,0); Serial.print("k on pin "); Serial.println(PIN_100K);
+  Serial.print("R = "); Serial.print(REF_1M,0); Serial.print("k on pin "); Serial.println(PIN_1M);
+  Serial.println("Available modes: C R U D");
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(PIN_1K, INPUT);
+  pinMode(PIN_10K, INPUT);
+  pinMode(PIN_100K, INPUT);
+  pinMode(PIN_1M, INPUT);
+}
+
+int meas_490Hz_step(byte pin) {
+  int i, j, v, vmin, vmax, result;
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, HIGH);
+  for(i = 0; i < 10000; i++) {
+    v = analogRead(PIN_IN);
+    if(v > 511) break;
+  }
+  digitalWrite(pin, LOW);
+  for(i = 0; i < 10000; i++) {
+    v = analogRead(PIN_IN);
+    if(v < 512) break;
+  }
+  analogWrite(pin, 127);
+  delay(100);
+  result = 0;
+  for(i = 0; i < 32; i++) {
+    vmin = 1023; vmax = 0;
+    for(j = 0; j < 25; j++) {
+      v = analogRead(PIN_IN);
+      vmin = min(vmin, v);
+      vmax = max(vmax, v);
+    }
+    result += vmax - vmin;
+  }
+  pinMode(pin, INPUT);
+  return result >> 5;
+}
+
+float meas_490Hz() {
+  int value;
+  value = meas_490Hz_step(PIN_1K);
+  if(value < 200) return 600000.0 / value;
+  value = meas_490Hz_step(PIN_10K);
+  if(value < 200) return 60000.0 / value;
+  value = meas_490Hz_step(PIN_100K);
+  if(value < 200) return 6000.0 / value;
+  else return 0.0;
+}
+
+long meas_C_step(byte pin) {
+  int i, v1, v2;
+  long t1, t2;
+  pinMode(pin, OUTPUT);  
+  digitalWrite(pin, HIGH);
+  for(i = 0; i < 10000; i++) {
+    t1 = micros();
+    v1 = analogRead(PIN_IN);
+    if(v1 > 767) break;
+  }
+  digitalWrite(pin, LOW);
+  for(i = 0; i < 10000; i++) {
+    t2 = micros();
+    v2 = analogRead(PIN_IN);
+    if(v2 < 282) break;
+  }
+  pinMode(pin, INPUT);
+  if((v1-v2) > 485+64) return 0;
+  else if((v1-v2) < 485-64) return -1;
+  return (t2-t1)*485/(v1-v2);
+}
+
+float meas_C() {
+  long value = 0;
+  value = meas_C_step(PIN_1K);
+  if(value > 0) return value/REF_1K;
+  value = meas_C_step(PIN_10K);
+  if(value > 0) return value/REF_10K;
+  value = meas_C_step(PIN_100K);
+  if(value > 0) return value/REF_100K;
+  value = meas_C_step(PIN_1M);
+  if(value > 0) return value/REF_1M;
+  else return 0.0;
+}
+
+int meas_R_step(byte pin) {
+  int value;
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, HIGH);
+  delay(1);
+  value = analogRead(PIN_IN);
+  pinMode(pin, INPUT);
+  return value;
+}
+
+float meas_R() {
+  long value = 0;
+  value = meas_R_step(PIN_1K);
+  if(value < 768) return value*REF_1K/(1023.0 - value);
+  value = meas_R_step(PIN_10K);
+  if(value < 768) return value*REF_10K/(1023.0 - value);
+  value = meas_R_step(PIN_100K);
+  if(value < 768) return value*REF_100K/(1023.0 - value);
+  value = meas_R_step(PIN_1M);
+  if(value < 1000) return value*REF_1M/(1023.0 - value);
+  else return 0.0;
+}
+
+float meas_U() {
+  int value = analogRead(PIN_IN);
+  return value * REF_V / 1023.0;
+}
+
+float meas_D() {
+  int value;
+  pinMode(PIN_1K, OUTPUT);
+  digitalWrite(PIN_1K, HIGH);
+  delay(1);
+  value = analogRead(PIN_IN);
+  pinMode(PIN_1K, INPUT);
+  if(value < 1020) return value * REF_V / 1023.0;
+  else return 0.0;
+}
+
+void loop() {
+  static byte cmd = 'U';
+  static float(*meas)() = meas_U;
+  static char *unit = "V";
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.write(cmd); Serial.print(" = "); Serial.print(meas()); Serial.println(unit);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  if(Serial.available() > 0) {
+    cmd = Serial.read() & 0xDF; // uppercase
+    switch(cmd) {
+      case 'C': meas = meas_C; unit = "nF";break;
+      case 'R': meas = meas_R; unit = "kOhm"; break;
+      case 'U': meas = meas_U; unit = "V";break;
+      case 'D': meas = meas_D; unit = "V";break;
+      default: cmd = 'U'; meas = meas_U; unit = "V";
+    }
+  }
+}
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,116 @@
-# CRUD_meter
-Measurement software for Arduino
+
+CRUD meter for Arduino
+======================
+
+CRUD meter lets you perform basic measurements using Arduino. It uses UART
+to connect to Serial Monitor, from which it gets commands to set measurement
+mode and where it outputs measurement results.
+
+'C', 'R', 'U' and 'D' are the measurement modes which can be selected
+by sending the corresponding character, hence the name.
+
+Available measurement modes include capacitance, resistance and voltage measurement with an additional mode
+designed to measure voltage drop on diodes, including LEDs.
+
+Schematic
+---------
+
+The hardware setup consists of an Arduino and 4 resistors with nominals in x10 increments.
+
+    Arduino
+    ────────┐
+      PIN_1K├──┤ 1k ├──┐
+            │          │
+     PIN_10K├──┤ 10k├──┤
+            │          │
+    PIN_100K├──┤100k├──┤
+            │          │
+      PIN_1M├──┤ 1M ├──┤
+            │          │  (+) (-)
+      PIN_IN├──────────┴───Ø   Ø───┐
+    ────────┘                     ─┴─
+                                  GND
+
+PIN_IN has to be an analog pin (A0 and up), other pins can be chosen freely.
+
+Resistors don't have to have precise values: a deviation of 20% is possible.
+For example, you can use anything between 0.8kOhm and 1.2kOhm as 1K resistor.
+
+If you don't have a 1M resistor, you can use a second 100K resistor instead.
+You'll get reduced measurement range though: you won't be able to measure
+resistance above 1MOhm and capacitance below 10nF.
+
+Calibration
+-----------
+
+Before you can use CRUD for actual measurements, you need to calibrate it.
+Measure the exact values of the resistors you're about to use (preferably
+before soldering if you're going to solder them), and set the calibration
+constants to the exact value you have used:
+
+REF_1K   - to the actual resistance of 1kOhm resistor
+REF_10K  - to the actual resistance of 10kOhm resistor
+REF_100K - to the actual resistance of 100kOhm resistor
+REF_1M   - to the actual resistance of 1MOhm resistor
+
+You will also need to measure the exact voltage on Arduino's VCC pin
+and assign that value to the calibration constant REF_V.
+
+Measurement
+-----------
+
+After a reset CRUD goes into 'U' mode, where it measures the voltage.
+Sending a single letter via the UART will switch to the corresponding mode.
+
+C - Capacitance
+---------------
+
+In this mode CRUD measures the RC time constant, trying different resistors from
+1k to 1M, until the time constant comes in range. Corresponding pins are driven
+HIGH and LOW to change and discharge the capacitor.
+
+Capacitance values in range from approx. 1 nF to 250 uF can be measured.
+Zero is displayed for values out of this range.
+
+Note: ceramic caps may require a long time for the measurement to stabilize.
+This is due to the fact that such caps are temperature-sensitive, and you have
+likely been holding them in your hands just before the measurement.
+
+Warning: polar capacitors (e.g. electrolytic) can be damaged if the polarity
+is not respected. Negative terminal should be connected to ground.
+
+R - Resistance
+--------------
+
+In this mode CRUD creates voltage dividers trying different resistors from
+1k to 1M, until the measured voltage is in range. Corresponding pins are driven
+HIGH during the measurement.
+
+Resistance values in range from approx. 10 Ohm to 20 MOhm can be measured.
+Zero is displayed for values out of this range.
+
+U - Voltage
+-----------
+
+In this mode CRUD measures the voltage on PIN_IN, while load pins are not driven.
+
+Voltages in range from 0 V to REF_V can be measured.
+
+Warning: measured voltage should never be negative or exceed REF_V.
+Doing so will permanently damage the controller.
+
+D - Diode voltage drop
+----------------------
+
+In this mode CRUD measures the voltage on PIN_IN, while PIN_1K is driven HIGH.
+
+If a diode is connected in forward direction (cathode to ground), a current
+of 1-5 mA will flow through it, allowing to measure forward voltage drop of
+rectifying diodes and most LEDs.
+
+If a diode is connected in reverse direction, it can be checked for integrity.
+For Zener diodes with breakdown voltage below REF_V, the breakdown voltage will
+be measured.
+
+Voltages in range from 0 V to 90% of REF_V can be measured.
+Zero is displayed for values out of this range.
